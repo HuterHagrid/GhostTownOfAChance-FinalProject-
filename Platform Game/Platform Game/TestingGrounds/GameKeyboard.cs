2bd99085af82923811d5e51d0cbe48b8c9502059
Hunter Haggard <1hunterh@gmail.com>
2019-11-04 13:25:50 -0800

    Added working test program y (and child classes)

---
+++ Platform Game/Platform Game/TestingGrounds/GameKeyboard.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestingGrounds
+{
+    class GameKeyboard
+    {
+        public KeyboardState newKeyboard, oldKeyboard;
+    }
+}
