2a1c0b65c1be855cb8769509f0aaa8f9965c6fab
Hunter Haggard <1hunterh@gmail.com>
2019-11-04 17:52:46 -0800

    This should work

---
+++ Platform Game/Platform Game/TestPlatformer/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace TestPlatformer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.screen = new System.Windows.Forms.Panel();
+            this.block1 = new System.Windows.Forms.PictureBox();
+            this.block = new System.Windows.Forms.PictureBox();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.screen.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.block1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.block)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // screen
+            // 
+            this.screen.BackColor = System.Drawing.Color.Black;
+            this.screen.Controls.Add(this.block1);
+            this.screen.Controls.Add(this.block);
+            this.screen.Controls.Add(this.player);
+            this.screen.Location = new System.Drawing.Point(0, 0);
+            this.screen.Margin = new System.Windows.Forms.Padding(2);
+            this.screen.Name = "screen";
+            this.screen.Size = new System.Drawing.Size(533, 278);
+            this.screen.TabIndex = 0;
+            // 
+            // block1
+            // 
+            this.block1.BackColor = System.Drawing.Color.Transparent;
+            this.block1.BackgroundImage = global::TestPlatformer.Properties.Resources.block;
+            this.block1.Location = new System.Drawing.Point(32, 228);
+            this.block1.Margin = new System.Windows.Forms.Padding(2);
+            this.block1.Name = "block1";
+            this.block1.Size = new System.Drawing.Size(187, 10);
+            this.block1.TabIndex = 2;
+            this.block1.TabStop = false;
+            this.block1.Tag = "platform";
+            // 
+            // block
+            // 
+            this.block.BackColor = System.Drawing.Color.Transparent;
+            this.block.BackgroundImage = global::TestPlatformer.Properties.Resources.block;
+            this.block.Location = new System.Drawing.Point(316, 228);
+            this.block.Margin = new System.Windows.Forms.Padding(2);
+            this.block.Name = "block";
+            this.block.Size = new System.Drawing.Size(187, 10);
+            this.block.TabIndex = 1;
+            this.block.TabStop = false;
+            this.block.Tag = "platform";
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Transparent;
+            this.player.Image = global::TestPlatformer.Properties.Resources.stand_right;
+            this.player.Location = new System.Drawing.Point(113, 72);
+            this.player.Margin = new System.Windows.Forms.Padding(2);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(16, 21);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 1;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.BackgroundImage = global::TestPlatformer.Properties.Resources.ground;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 257);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(533, 35);
+            this.panel1.TabIndex = 2;
+            this.panel1.Tag = "platform";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(533, 292);
+            this.Controls.Add(this.screen);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "GhostTown";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.screen.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.block1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.block)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel screen;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.PictureBox block;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox block1;
+    }
+}
+
---
+++ Platform Game/Platform Game/TestPlatformer/Form1.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestPlatformer
+{
+    public partial class Form1 : Form
+    {
+        private const int FORCE = 12;
+        private const int JUMP_SPEED = 10;
+
+        bool left = false;
+        bool right = false;
+        bool jump = false;
+
+        int force = FORCE;
+        int index = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            player.Top = screen.Height - player.Height;
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            index++;
+            //gif replay
+            if(right == true && index % 15 == 0)
+            {
+                player.Image = Image.FromFile("run_right.gif");
+            }
+            if (left == true && index % 15 == 0)
+            {
+                player.Image = Image.FromFile("run_left.gif");
+            }
+
+            // Side Collision 
+            if (player.Right > block.Left && 
+                player.Left < block.Right - player.Width
+                && player.Bottom < block.Bottom && player.Bottom > block.Top)
+            {
+                right = false;
+            }
+            //side collision
+            if (player.Left < block.Right &&
+                player.Right > block.Left + player.Width
+                && player.Bottom < block.Bottom && player.Bottom > block.Top)
+            {
+                left = false;
+            } 
+            //2.
+            if (right == true)
+            {
+                player.Left += 2;
+            }
+            if (left == true)
+            {
+                player.Left -= 2;
+            }
+
+            if (jump == true)
+            {
+                //falling(if the player has jumped before)
+                player.Top -= force;
+                force -= 1;
+            }
+            if(player.Top + player.Height >= screen.Height)
+            {
+                player.Top = screen.Height - player.Height; //stop falling at bottom
+                if (jump == true)
+                {
+                    player.Image = Image.FromFile("stand_right.png");
+                }
+                jump = false;
+            }
+            else //falling
+            {
+                player.Top += 2;
+            }
+
+            foreach (Control x in this.Controls)
+            {
+                // Keep player on platform
+                if (x is PictureBox && x.Tag == "platform")
+                {
+                    if (player.Bounds.IntersectsWith(x.Bounds) && !jump)
+                    {
+                        force = FORCE;
+                        player.Top = x.Top - player.Height;
+                    }
+                }
+
+                //
+                if (x is PictureBox && x.Tag == "platform_bottom")
+                {
+                    if (player.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        player.Top = x.Top + player.Height;
+                    }
+                }
+
+                // Player cannot go through edges
+                if (x is PictureBox && x.Tag == "edge")
+                {
+                    if (player.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        if (left)
+                        {
+                            player.Left = x.Left + player.Width;
+                        }
+                        if (right)
+                        {
+                            player.Left = x.Left - player.Width;
+                        }
+                    }
+                }
+            }
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(jump != true)
+            {
+                if(e.KeyCode == Keys.Space)
+                {
+                    jump = true;
+                    force = FORCE;
+                    player.Image = Image.FromFile("jump_right.png");
+                }
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                left = true;
+                right = false;
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                right = true;
+                left = false;
+            }
+            if (e.KeyCode == Keys.Escape) { this.Close(); } // Escape -> Exit
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right)
+            {
+                right = false;
+                player.Image = Image.FromFile("stand_right.png");
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                left = false;
+                player.Image = Image.FromFile("stand_left.png");
+            }
+        }
+
+
+
+        /*
+         
+
+        private const int FORCE = 8;
+        private const int JUMP_SPEED = 10;
+
+        bool goleft = false;
+        bool goright = false;
+        bool jumping = false;
+
+        int jumpspeed = JUMP_SPEED;
+        int movespeed = 0;
+        int force = FORCE;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goleft = true;
+                goright = false;
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                goright = true;
+                goleft = false;
+            }
+
+            if (e.KeyCode == Keys.Space && !jumping)
+            {
+                jumping = true;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goleft = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goright = false;
+            }
+            if (jumping)
+            {
+                jumping = false;
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            player.Top += jumpspeed;
+
+            if (jumping && force < 0)
+            {
+                jumping = false;
+            }
+
+            if (goleft)
+            {
+                if (movespeed < 10)
+                    movespeed += 1;
+                player.Left -= movespeed;
+                player.Image = Image.FromFile("run_right.gif");
+            }
+            else if (goright)
+            {
+                if (movespeed < 10)
+                    movespeed += 1;
+                player.Left += movespeed;
+                player.Image = Image.FromFile("run_left.gif");
+            }
+            else if (!goleft && !goright)
+            {
+                if (movespeed > 0)
+                    movespeed -= 1;
+            }
+
+            if (jumping)
+            {
+                jumpspeed = -1 * JUMP_SPEED;
+                force -= 1;
+            }
+            else
+            {
+                jumpspeed = JUMP_SPEED;
+            }
+
+            // Collision Check
+            foreach (Control x in this.Controls)
+            {
+                // Keep player on platform
+                if (x is PictureBox && x.Tag == "platform")
+                {
+                    if (player.Bounds.IntersectsWith(x.Bounds) && !jumping)
+                    {
+                        force = FORCE;
+                        player.Top = x.Top - player.Height;
+                    }
+                }
+
+                //
+                if (x is PictureBox && x.Tag == "platform_bottom")
+                {
+                    if (player.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        player.Top = x.Top + player.Height;
+                    }
+                }
+
+                // Player cannot go through edges
+                if (x is PictureBox && x.Tag == "edge")
+                {
+                    if (player.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        if (goleft)
+                        {
+                            player.Left = x.Left + player.Width;
+                        }
+                        if (goright)
+                        {
+                            player.Left = x.Left - player.Width;
+                        }
+                    }
+                }
+            }
+            // End of Collision Check
+            //player.Image = Image.FromFile("run_right.gif");
+        }
+    }
+
+         */
+
+
+    }
+}
