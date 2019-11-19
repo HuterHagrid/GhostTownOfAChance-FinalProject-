aba31453084e6a7167984eb8db04e244d1a322e4
Hunter Haggard <1hunterh@gmail.com>
2019-11-18 21:07:56 -0800

    blech instead of exiting the app when game over. ore at end)

--- Platform Game/Platform Game/PlatformerProject/Form1.Designer.cs
+++ Platform Game/Platform Game/PlatformerProject/Form1.Designer.cs
@@ -69,7 +69,7 @@
             this.ghostie.BackColor = System.Drawing.Color.Transparent;
             this.ghostie.Image = global::PlatformerProject.Properties.Resources.ghostLeft;
             this.ghostie.Location = new System.Drawing.Point(264, 454);
-            this.ghostie.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.ghostie.Margin = new System.Windows.Forms.Padding(2);
             this.ghostie.Name = "ghostie";
             this.ghostie.Size = new System.Drawing.Size(14, 14);
             this.ghostie.TabIndex = 13;
@@ -81,7 +81,7 @@
             this.pictureBox6.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox6.BackgroundImage = global::PlatformerProject.Properties.Resources.block;
             this.pictureBox6.Location = new System.Drawing.Point(59, 454);
-            this.pictureBox6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox6.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox6.Name = "pictureBox6";
             this.pictureBox6.Size = new System.Drawing.Size(144, 6);
             this.pictureBox6.TabIndex = 12;
@@ -93,7 +93,7 @@
             this.pictureBox9.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox9.BackgroundImage = global::PlatformerProject.Properties.Resources.block;
             this.pictureBox9.Location = new System.Drawing.Point(59, 444);
-            this.pictureBox9.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox9.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox9.Name = "pictureBox9";
             this.pictureBox9.Size = new System.Drawing.Size(144, 16);
             this.pictureBox9.TabIndex = 11;
@@ -104,7 +104,7 @@
             // 
             this.tortise.BackColor = System.Drawing.Color.DarkGreen;
             this.tortise.Location = new System.Drawing.Point(147, 419);
-            this.tortise.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tortise.Margin = new System.Windows.Forms.Padding(2);
             this.tortise.Name = "tortise";
             this.tortise.Size = new System.Drawing.Size(27, 21);
             this.tortise.TabIndex = 10;
@@ -116,7 +116,7 @@
             this.pictureBox5.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox5.BackgroundImage = global::PlatformerProject.Properties.Resources.block;
             this.pictureBox5.Location = new System.Drawing.Point(220, 502);
-            this.pictureBox5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox5.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox5.Name = "pictureBox5";
             this.pictureBox5.Size = new System.Drawing.Size(144, 6);
             this.pictureBox5.TabIndex = 9;
@@ -128,7 +128,7 @@
             this.pictureBox8.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox8.BackgroundImage = global::PlatformerProject.Properties.Resources.block;
             this.pictureBox8.Location = new System.Drawing.Point(220, 491);
-            this.pictureBox8.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox8.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox8.Name = "pictureBox8";
             this.pictureBox8.Size = new System.Drawing.Size(144, 16);
             this.pictureBox8.TabIndex = 8;
@@ -139,7 +139,7 @@
             // 
             this.pictureBox7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
             this.pictureBox7.Location = new System.Drawing.Point(409, 532);
-            this.pictureBox7.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox7.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox7.Name = "pictureBox7";
             this.pictureBox7.Size = new System.Drawing.Size(136, 6);
             this.pictureBox7.TabIndex = 7;
@@ -148,12 +148,12 @@
             // 
             // coin
             // 
-            this.coin.BackColor = System.Drawing.Color.White;
+            this.coin.BackColor = System.Drawing.Color.Transparent;
             this.coin.Image = global::PlatformerProject.Properties.Resources.Coin;
             this.coin.Location = new System.Drawing.Point(486, 491);
-            this.coin.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.coin.Margin = new System.Windows.Forms.Padding(2);
             this.coin.Name = "coin";
-            this.coin.Size = new System.Drawing.Size(29, 26);
+            this.coin.Size = new System.Drawing.Size(9, 11);
             this.coin.TabIndex = 6;
             this.coin.TabStop = false;
             this.coin.Tag = "coin";
@@ -163,7 +163,7 @@
             this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox4.BackgroundImage = global::PlatformerProject.Properties.Resources.block;
             this.pictureBox4.Location = new System.Drawing.Point(405, 521);
-            this.pictureBox4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox4.Name = "pictureBox4";
             this.pictureBox4.Size = new System.Drawing.Size(144, 12);
             this.pictureBox4.TabIndex = 4;
@@ -175,7 +175,7 @@
             this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox3.BackgroundImage = global::PlatformerProject.Properties.Resources.wall;
             this.pictureBox3.Location = new System.Drawing.Point(1, -1);
-            this.pictureBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox3.Name = "pictureBox3";
             this.pictureBox3.Size = new System.Drawing.Size(23, 588);
             this.pictureBox3.TabIndex = 3;
@@ -187,7 +187,7 @@
             this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox2.BackgroundImage = global::PlatformerProject.Properties.Resources.wall;
             this.pictureBox2.Location = new System.Drawing.Point(628, -1);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(23, 588);
             this.pictureBox2.TabIndex = 2;
@@ -199,7 +199,7 @@
             this.player.BackColor = System.Drawing.Color.Transparent;
             this.player.Image = global::PlatformerProject.Properties.Resources.stand_right;
             this.player.Location = new System.Drawing.Point(99, 514);
-            this.player.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.player.Margin = new System.Windows.Forms.Padding(2);
             this.player.Name = "player";
             this.player.Size = new System.Drawing.Size(16, 21);
             this.player.TabIndex = 1;
@@ -210,7 +210,7 @@
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.BackgroundImage = global::PlatformerProject.Properties.Resources.ground;
             this.pictureBox1.Location = new System.Drawing.Point(1, 582);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(651, 32);
             this.pictureBox1.TabIndex = 0;
@@ -236,7 +236,7 @@
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.player);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
